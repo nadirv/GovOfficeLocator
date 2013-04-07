82544f5233349383a660ac69bba9b0813f1ad621
Nadir Viqar <nadirviqar@msn.com>
2013-04-07 18:02:01 +0500

    First prototype

---
+++ Gov/Default.aspx
@@ -0,0 +1,59 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CivicHackWebAPI.Gov.Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Government Services</title>
+<%--    <link href="../Scripts/leaflet.css" rel="stylesheet" type="text/css" />
+    <link href="../Scripts/leaflet.ie.css" rel="stylesheet" type="text/css" />
+    <script src="../Scripts/leaflet.js" type="text/javascript"></script>
+--%>
+    <script src="../Scripts/jquery-1.4.1.js" type="text/javascript"></script>
+    
+    <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.5.1/leaflet.css" />
+    <script src="http://cdn.leafletjs.com/leaflet-0.5.1/leaflet.js"></script>
+
+    <script src="../Scripts/GovMap.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            onMapLoaded();
+        });
+
+        function onMapLoaded() {
+            naqsha.LoadMap();
+        }
+
+        function searchGovOffice() {
+            var searchSubCat = document.getElementById('selCategory').value;
+            
+            naqsha.SearchGovOffice(searchSubCat);
+        }
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="header">
+    <h1>Government Offices & Services Locator</h1>
+        Select Category
+        <select id="selCategory" onchange="searchGovOffice()">
+            <option selected="selected">Select</option>
+            <option value="Fedral Government Office">Federal Government Office</option>
+            <%--<option value="Provincial Government Office">Provincial Government Office</option>--%>
+            <option value="Local Government Office">Local Government Office</option>
+            <option value="City Water">Water & Sewerage</option>
+            <option value="Police Station">Police Station</option>
+            <option value="Fire Station">Fire Station</option>
+            <option value="Law Court">Courts</option>
+            <option value="Nazim">Nazim Office</option>
+            <option value="UC">Union Council Office</option>
+            <option value="Post">Post Office</option>
+        </select>
+    </div>
+    <div id="resultList" style="float: left; font-family: Arial; font-size:small; width: 200px;">
+        
+    </div>
+    <div id="theMap" style="width: 800px; height: 600px; margin-left: 202px;">
+    </div>
+    </form>
+</body>
+</html>
---
+++ Gov/GovMap.js
@@ -0,0 +1,88 @@
+﻿function naqsha() { }
+
+naqsha.MapDivId = 'theMap';
+naqsha.TileURL = 'http://tiles.naqsha.net/Tiles/{z}/{x}/{y}.png';
+naqsha.Attribution = 'Map data &copy; 2012 Data Solutions Private Limited';
+naqsha._map = null;
+naqsha._markers = [];
+naqsha._results = [];
+naqsha._waypoints = [];
+naqsha._selected = null;
+naqsha.MarkerLayer = null;
+naqsha.MarkerBounds = null;
+
+naqsha.LoadMap = function (latitude, longitude) {
+    naqsha._map = new L.Map(naqsha.MapDivId);
+
+    var naqshaTileLayer = new L.TileLayer(naqsha.TileURL, { maxZoom: 17, attribution: naqsha.Attribution });
+    naqsha._map.addLayer(naqshaTileLayer);
+
+    if (latitude != null && longitude != null) {
+        var center = new L.LatLng(latitude, longitude);
+    }
+
+    //naqsha._map.setView(new L.LatLng(33.7, 73.0), 13);    //Islamabad
+    naqsha._map.setView(new L.LatLng(24.9, 67.06), 11);    //Karachi
+
+    naqsha.MarkerLayer = new L.LayerGroup();
+    naqsha._map.addLayer(naqsha.MarkerLayer);
+
+    naqsha.RouteLayer = new L.LayerGroup();
+    naqsha._map.addLayer(naqsha.RouteLayer);
+}
+
+naqsha.ClearMarkers = function () {
+    naqsha.MarkerLayer.clearLayers();
+    naqsha._markers.length = 0;
+}
+
+naqsha.LoadPin = function (i, e) {
+    var LL = new L.LatLng(e.Latitude, e.Longitude);
+    var marker = new L.Marker(LL);
+    naqsha.MarkerLayer.addLayer(marker);
+    marker.bindPopup(generatePopupHTML(i, e));
+    naqsha._markers.push(marker);
+    naqsha._results.push(e);
+    naqsha.MarkerBounds.extend(LL);
+
+    function generatePopupHTML(i, e) {
+        var HTML = "<div id='divPopup'><b>" + e.Name + "</b></br>" + e.Address + "</br></div>";
+        return HTML;
+    }
+}
+naqsha.SearchGovOffice = function (searchCategory) {
+   $.ajax({
+       type: "POST",
+       url: "../WebService.asmx/GetGovOfficeByCategory",
+       data: "{ 'Category': '" + searchCategory + "' }",
+       contentType: "application/json; charset=utf-8",
+       dataType: "json",
+       success: function (data) {
+           naqsha._renderGovOfficeResults(data["d"]);
+       }
+   });
+}
+
+naqsha._renderGovOfficeResults = function (response) {
+    $("#resultList").empty();
+    if (response.length > 0) {
+        naqsha._results = [];
+        naqsha.ClearMarkers();
+        naqsha.MarkerBounds = new L.LatLngBounds();
+
+        naqsha._map.removeLayer(naqsha.RouteLayer);
+        naqsha._map.addLayer(naqsha.MarkerLayer);
+
+        var internalString = '<div style="font-size: small;">';
+        $.each(response, function (i, result) {
+            naqsha.LoadPin(i, result);
+            internalString += '<li><b>' + result.Name + '</b><br/>' + result.Address + '</li>';
+        });
+        $("#resultList").html(internalString + '</div>');        
+    }
+    else {
+        var message = '<p><b>No match found</b></p>';
+        $('#resultList').html(message);
+    }
+}
+
